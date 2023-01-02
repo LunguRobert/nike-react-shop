81e19a78cd6a9264a75d4b74b0b22eb3733abddf
LunguRobert <99295585+LunguRobert@users.noreply.github.com>
2023-01-02 20:28:13 +0200

    Add files via upload

---
+++ src/configs/firebase.js
@@ -0,0 +1,14 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBw9PJoTEoPkTqGs6jk6wR3q3mBMNbPJfA",
+    authDomain: "the-elegant-shop.firebaseapp.com",
+    projectId: "the-elegant-shop",
+    storageBucket: "the-elegant-shop.appspot.com",
+    messagingSenderId: "853701311589",
+    appId: "1:853701311589:web:e37fdd71422eedfa0b9a8c",
+    measurementId: "G-F2JV38C6F2"
+  };
+
+
+  export default firebaseConfig;
